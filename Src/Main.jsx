b371b2c7e39bc0bb07c70ebc83def9895c2fa933
KPetnicki7 <krispetni@live.com>
2026-05-17 11:15:42 -0700

    Delete Src/Main.jsx

--- Src/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TrainingPlan from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TrainingPlan />
-  </React.StrictMode>,
-)
