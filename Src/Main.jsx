fa37ddedde9574dc776ec82a72ee32f241a63d7a
KPetnicki7 <krispetni@live.com>
2026-05-17 10:51:16 -0700

    Initialize Main.jsx with React and TrainingPlan

---
+++ Src/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TrainingPlan from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrainingPlan />
+  </React.StrictMode>,
+)
